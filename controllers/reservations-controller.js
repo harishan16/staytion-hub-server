0872a59c4537e6ec5693314c181e10373aabb5e6
Viswa Harini Vidyashankar <viswavidya16@gmail.com>
2024-10-22 11:36:34 -0400

    get list and find details

---
+++ controllers/reservations-controller.js
@@ -0,0 +1,39 @@
+import initKnex from "knex";
+import configuration from "../knexfile.js";
+// import { matchedData } from "express-validator";
+const knex = initKnex(configuration);
+
+//index get
+
+const index = async (req, res) => {
+    try {
+        const reservationsList = await knex("reservations");
+
+        if (reservationsList.length === 0) {
+            return res.status(404).json({
+                message: "Reservations list not found",
+            });
+        } else {
+                res.status(200).json(reservationsList);
+        }
+
+    } catch (error){
+        res.status(500).json({
+            message: "Unable to retrieve reservations list",
+          });
+    }
+}
+
+const viewDetail = async (req, res) => {
+    try {
+        console.log(req);
+
+    } catch (error) {
+        res.status(500).json ({
+            message: `Unable to retrieve details of reservation ${id}`
+        })
+
+    }
+}
+
+export { index };
